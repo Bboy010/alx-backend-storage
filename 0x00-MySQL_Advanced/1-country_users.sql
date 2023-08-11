a46ab7b6ec6bd0e13c6b09f32388ead146ab139c
Bboy010 <hkoffianderson@gmail.com>
2023-08-10 18:44:42 -0700

    In and not out

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,9 @@
+-- Create table users
+-- add country enumerate US,CO,TN
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+    PRIMARY KEY (id)
+);
