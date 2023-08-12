4a8192868a3cbf6d7f85320474398b364c4c5d24
Bboy010 <hkoffianderson@gmail.com>
2023-08-12 05:45:59 -0700

    upsate

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 -- As their main style, ranked by their longevity
 SELECT band_name, (split - formed) AS lifespan
 FROM metal_bands
-WHERE style = 'Glam Rock'
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC;
