9710aa9646f31733c0335270c85dfe31c38c0a02
Bboy010 <hkoffianderson@gmail.com>
2023-08-12 04:10:19 -0700

    Best band ever!

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,6 @@
 -- Column names must be: origin and nb_fans
 -- nb_fans is the number of fans in this country
-SELECT origin, COUNT(*) AS nb_fans
+SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY nb_fans DESC, origin ASC;
+ORDER BY nb_fans DESC;
